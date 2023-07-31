8aa90351cdc9dcae77edd3c590f7529d996c62c7
Peer Richelsen <peer@cal.com>
2023-07-31 20:45:55 +0200

    nit footer fix

--- website/src/components/ContactSection.jsx
+++ website/src/components/ContactSection.jsx
@@ -7,7 +7,7 @@ import { ContactButtons } from './CTA'
 export function ContactSection() {
   return (
     <Container id="contact-us" className="mt-24 sm:mt-32 lg:mt-40">
-      <FadeIn className="-mx-6 rounded-4xl bg-neutral-950 px-6 py-20 sm:mx-0 sm:py-32 md:px-12">
+      <FadeIn className="-mx-6 bg-neutral-950 px-6 py-20 sm:mx-0 sm:rounded-4xl sm:py-32 md:px-12">
         <div className="mx-auto max-w-4xl">
           <div className="max-w-xl">
             <h2 className="font-display text-3xl font-medium text-white [text-wrap:balance] sm:text-4xl">
