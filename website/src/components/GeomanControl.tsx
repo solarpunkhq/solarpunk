18007cd0f27a592764b5175990d7ec327f0f2b8e
Peer Richelsen <peer@cal.com>
2024-04-28 10:22:40 +0200

    fixed import

--- website/src/components/GeomanControl.tsx
+++ website/src/components/GeomanControl.tsx
@@ -1,31 +1,31 @@
-import { createControlComponent } from "@react-leaflet/core";
-import * as L from "leaflet";
-import "@geoman-io/leaflet-geoman-free";
-import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
+import { createControlComponent } from '@react-leaflet/core'
+import * as L from 'leaflet'
+import '@geoman-io/leaflet-geoman-pro'
+import '@geoman-io/leaflet-geoman-pro/dist/leaflet-geoman.css'
 
 interface Props extends L.ControlOptions {
-  position: L.ControlPosition;
-  drawCircle?: boolean;
-  oneBlock?: boolean;
+  position: L.ControlPosition
+  drawCircle?: boolean
+  oneBlock?: boolean
 }
 
 const Geoman = L.Control.extend({
   options: {},
   initialize(options: Props) {
-    L.setOptions(this, options);
+    L.setOptions(this, options)
   },
 
   addTo(map: L.Map) {
-    if (!map.pm) return;
+    if (!map.pm) return
 
     map.pm.addControls({
       ...this.options,
-    });
+    })
   },
-});
+})
 
 const createGeomanInstance = (props: Props) => {
-  return new Geoman(props);
-};
+  return new Geoman(props)
+}
 
-export const GeomanControl = createControlComponent(createGeomanInstance);
+export const GeomanControl = createControlComponent(createGeomanInstance)
