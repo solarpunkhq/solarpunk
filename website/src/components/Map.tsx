8ceb84870ab0142d5bd577c5a8e8346146489710
Peer Richelsen <peer@cal.com>
2024-05-29 11:52:03 +0200

    new key

--- website/src/components/Map.tsx
+++ website/src/components/Map.tsx
@@ -25,7 +25,7 @@ export default function Map() {
       >
         <TileLayer
           attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-          url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=FTejVijNkqKWPbQui8i9"
+          url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=sYQTqz7Q6H7zcmIU8B4d"
         />
 
         <GeomanControl
