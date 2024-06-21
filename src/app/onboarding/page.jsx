45a1fa40af37f14d81bdee0cb5be7d38c308ce26
Abhijeet Singh <asingh9829@gmail.com>
2024-06-22 01:06:14 +0530

    fix: changed ( class-> className )

--- src/app/onboarding/page.jsx
+++ src/app/onboarding/page.jsx
@@ -15,50 +15,50 @@ export default function Contact() {
           <div className="text-center">
             <h1 className="font-display text-5xl">Mark your acres</h1>
             <h2 className="">Use the map to outline your territory</h2>
-            <div class="relative overflow-x-auto">
-              <table class="mt-4 w-full border text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
-                <thead class="bg-gray-50 text-xs uppercase text-gray-700 ">
+            <div className="relative overflow-x-auto">
+              <table className="mt-4 w-full border text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
+                <thead className="bg-gray-50 text-xs uppercase text-gray-700 ">
                   <tr>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                       Area
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                       Revenue/yr
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr class="border-b bg-white ">
+                  <tr className="border-b bg-white ">
                     <th
                       scope="row"
-                      class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
+                      className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
                     >
                       Area 01
                     </th>
-                    <td class="px-6 py-4">$700,000/yr</td>
+                    <td className="px-6 py-4">$700,000/yr</td>
                   </tr>
-                  <tr class="border-b bg-white ">
+                  <tr className="border-b bg-white ">
                     <th
                       scope="row"
-                      class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
+                      className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
                     >
                       Area 02
                     </th>
-                    <td class="px-6 py-4">$600,000/yr</td>
+                    <td className="px-6 py-4">$600,000/yr</td>
                   </tr>
-                  <tr class="bg-gray-50">
+                  <tr className="bg-gray-50">
                     <th
                       scope="row"
-                      class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
+                      className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
                     >
                       Total
                     </th>
-                    <td class="px-6 py-4">$1,300,00/yr</td>
+                    <td className="px-6 py-4">$1,300,00/yr</td>
                   </tr>
                 </tbody>
               </table>
             </div>
-            <input className="mt-4 w-full rounded border px-4" value="Email" />
+            <input className="mt-4 w-full rounded border px-4" placeholder="Email" />
             <Button className="mx-auto mt-4">Submit Info</Button>
           </div>
         </aside>
