299ddca20c2c78dbce84eff7875894ea999cbe5b
Varun Balani <varunprahladb@gmail.com>
2024-06-22 19:32:23 +0530

    save to db

--- src/app/api/submit_onboarding/route.ts
+++ src/app/api/submit_onboarding/route.ts
@@ -1,7 +1,23 @@
 import { NextResponse } from 'next/server'
+import prisma from '@/lib/prisma'
 
 export async function POST(request: Request) {
   const body = await request.json()
   console.log(body)
-  return NextResponse.json({ message: 'Hello World' }, { status: 200 })
+
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      name: body.name,
+      acres: {
+        create: body.acres,
+      },
+      current_step: 1,
+    },
+  })
+
+  return NextResponse.json(
+    { message: 'Submitted Successfully' },
+    { status: 200 }
+  )
 }
---
+++ src/app/onboarding/thank_you/page.tsx
@@ -0,0 +1,10 @@
+export default function ThankYouPage() {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <h1 className="text-xl font-bold">
+        Thank you for submitting your acres.
+      </h1>
+      <div>We will be in touch with you within the next 24 hours</div>
+    </div>
+  )
+}
