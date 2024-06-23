c3de989a9b69cf9bf3abc63eadd419e15f2d888e
shanvy <iamshanvy@gmail.com>
2024-06-23 13:42:16 +0800

    google maps tile

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -37,8 +37,8 @@ export default function Map({ setOutlinedAreas }: { setOutlinedAreas: any }) {
         }}
       >
         <TileLayer
-          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-          url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=sYQTqz7Q6H7zcmIU8B4d"
+          attribution="Google Maps"
+          url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
         />
 
         <GeomanControl
