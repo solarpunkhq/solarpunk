6e838e58328c6b68818ef0fdf3891e52b5e2fae0
Varun Balani <varunprahladb@gmail.com>
2024-07-23 20:50:28 +0530

    testing

--- src/components/EditAcresForm.jsx
+++ src/components/EditAcresForm.jsx
@@ -70,7 +70,7 @@ export default function EditAcresForm({
           lng={lng}
           acres={acres}
           setAcres={setAcres}
-          existingAcres={existing_acres}
+          existingAcres={[]}
         />
         <div className="rounded-r-4xl h-full w-full max-w-96 border border-l-0 bg-white p-8">
           <div>
