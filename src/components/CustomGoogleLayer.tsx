2da8cbd642930af70e2a95a8fcf20a1d4484caf6
Varun Balani <varunprahladb@gmail.com>
2024-07-24 02:16:59 +0530

    bug fix attempt

--- src/components/CustomGoogleLayer.tsx
+++ src/components/CustomGoogleLayer.tsx
@@ -6,7 +6,7 @@ import {
   LeafletContextInterface,
   LayerProps,
 } from '@react-leaflet/core'
-import 'leaflet.gridlayer.googlemutant/dist/Leaflet.GoogleMutant'
+import 'leaflet.gridlayer.googlemutant'
 import { Loader, LoaderOptions } from '@googlemaps/js-api-loader'
 
 interface IGoogleMapsAddLayer {
@@ -24,6 +24,24 @@ interface IProps extends L.gridLayer.GoogleMutantOptions {
 
 let googleMapsScriptLoaded = false
 
+const loadGoogleMapsScript = (loaderConf: LoaderOptions) => {
+  return new Promise<void>((resolve, reject) => {
+    if (googleMapsScriptLoaded) {
+      resolve()
+      return
+    }
+
+    const loader = new Loader(loaderConf)
+    loader
+      .load()
+      .then(() => {
+        googleMapsScriptLoaded = true
+        resolve()
+      })
+      .catch(reject)
+  })
+}
+
 const createLeafletElement = (
   props: IProps,
   context: LeafletContextInterface
@@ -35,13 +53,9 @@ const createLeafletElement = (
     googleMapsAddLayers,
     ...googleMutantProps
   } = props
-  if (useGoogMapsLoader && !googleMapsScriptLoaded) {
-    const loader = new Loader({ apiKey, ...googleMapsLoaderConf })
-    loader.load()
-    googleMapsScriptLoaded = true
-  }
 
   const instance = L.gridLayer.googleMutant(googleMutantProps)
+
   if (googleMapsAddLayers) {
     googleMapsAddLayers.forEach((layer) => {
       ;(instance as L.gridLayer.GoogleMutant).addGoogleLayer(
@@ -50,10 +64,36 @@ const createLeafletElement = (
       )
     })
   }
+
   return { instance, context }
 }
 
-export default createLayerComponent<L.GridLayer, LayerProps & IProps>(
-  createLeafletElement,
-  updateGridLayer
-)
+const GoogleMutantLayer = createLayerComponent<
+  L.GridLayer,
+  LayerProps & IProps
+>(createLeafletElement, updateGridLayer)
+
+const GoogleMutantLayerWrapper: React.FC<IProps> = (props) => {
+  const [isScriptLoaded, setScriptLoaded] = React.useState(false)
+
+  React.useEffect(() => {
+    if (props.useGoogMapsLoader) {
+      loadGoogleMapsScript({
+        apiKey: props.apiKey,
+        ...props.googleMapsLoaderConf,
+      })
+        .then(() => setScriptLoaded(true))
+        .catch(console.error)
+    } else {
+      setScriptLoaded(true)
+    }
+  }, [props.apiKey, props.useGoogMapsLoader, props.googleMapsLoaderConf])
+
+  if (!isScriptLoaded) {
+    return null // or a loading spinner
+  }
+
+  return <GoogleMutantLayer {...props} />
+}
+
+export default GoogleMutantLayerWrapper
