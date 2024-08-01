baa4bf9e832ccf84b53c5a3f8bddf06d168528af
Varun Balani <varunprahladb@gmail.com>
2024-08-01 22:01:42 +0530

    fix font in admin dashboard

--- src/app/(admin)/layout.jsx
+++ src/app/(admin)/layout.jsx
@@ -1,5 +1,4 @@
 import { cn } from '@/lib/utils'
-import localFont from 'next/font/local'
 import '@/styles/tailwind.css'
 import { Inter } from 'next/font/google'
 import { Toaster } from '@/components/ui/toaster'
@@ -12,21 +11,12 @@ import {
 } from '@/components/ui/tooltip'
 import Link from 'next/link'
 import { Home, LineChart, Settings, SheetIcon, Users2 } from 'lucide-react'
-const calSans = localFont({
-  src: [
-    {
-      path: '../../../public/fonts/CalSans-SemiBold.ttf',
-      weight: '400',
-    },
-  ],
-  variable: '--font-cal-sans',
-})
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Layout({ children }) {
   return (
-    <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+    <html lang="en" className={'h-full bg-neutral-950 text-base antialiased'}>
       <head>
         <link
           rel="stylesheet"
@@ -51,9 +41,8 @@ export default function Layout({ children }) {
       </head>
       <body
         className={cn(
-          'flex min-h-screen flex-col bg-black font-sans antialiased',
-          calSans.variable,
-          inter.variable
+          'flex min-h-screen flex-col bg-black antialiased',
+          inter.className
         )}
       >
         <TooltipProvider>
