ed1905de4c54c0b1c1b9c86c1184c3d2cdb4bcd9
Varun Balani <varunprahladb@gmail.com>
2024-08-14 03:23:01 +0530

    switch to vercel analytics

--- src/app/(admin)/layout.jsx
+++ src/app/(admin)/layout.jsx
@@ -3,8 +3,7 @@ import '@/styles/tailwind.css'
 import { Inter } from 'next/font/google'
 import { Toaster } from '@/components/ui/toaster'
 import { TooltipProvider } from '@radix-ui/react-tooltip'
-import { GoogleAnalytics } from '@next/third-parties/google'
-
+import { Analytics } from '@vercel/analytics/react'
 import {
   Tooltip,
   TooltipContent,
@@ -100,7 +99,7 @@ export default function Layout({ children }) {
           <Toaster />
         </TooltipProvider>
       </body>
-      <GoogleAnalytics gaId="G-0LZEJ5S1H8" />
+      <Analytics />
     </html>
   )
 }
--- src/app/(user)/layout.jsx
+++ src/app/(user)/layout.jsx
@@ -5,7 +5,7 @@ import '@/styles/tailwind.css'
 import { Inter } from 'next/font/google'
 import { Toaster } from '@/components/ui/toaster'
 import { TooltipProvider } from '@radix-ui/react-tooltip'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
   title: {
@@ -64,7 +64,7 @@ export default function Layout({ children }) {
           <Toaster />
         </TooltipProvider>
       </body>
-      <GoogleAnalytics gaId="G-0LZEJ5S1H8" />
+      <Analytics />
     </html>
   )
 }
