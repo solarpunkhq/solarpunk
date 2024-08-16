ca91ba4a28af181d1de6e4fdcfcc22704bcc88d0
Varun Balani <varunprahladb@gmail.com>
2024-08-17 00:31:41 +0530

    send email when user updates acres

--- src/app/api/update_onboarding/route.ts
+++ src/app/api/update_onboarding/route.ts
@@ -3,6 +3,7 @@ import prisma from '@/lib/prisma'
 import { Resend } from 'resend'
 import { iso1A2Code } from '@rapideditor/country-coder'
 import { createClient } from '@/utils/supabase/server'
+import { SubmittedTemplate } from '@/email_templates/SubmittedTemplate'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -46,6 +47,13 @@ export async function POST(request: Request) {
     where: {
       email: body.email,
     },
+  })
+
+  const { data: data_two, error: error_two } = await resend.emails.send({
+    from: process.env.ONBOARDING_SEND_FROM_EMAIL,
+    to: process.env.ONBOARDING_ALERT_EMAIL,
+    subject: 'User Acres Updated',
+    react: SubmittedTemplate({ name: body.name }),
   })
 
   return NextResponse.json({ message: 'Updated Successfully' }, { status: 200 })
