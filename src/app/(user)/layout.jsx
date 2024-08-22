bf89abafe3eb7044ef8220a5fdd17fd1d98aa71b
Varun Balani <varunprahladb@gmail.com>
2024-08-22 12:58:34 +0530

    enable geoman scripts

--- src/app/(user)/layout.jsx
+++ src/app/(user)/layout.jsx
@@ -60,22 +60,20 @@ export default function Layout({ children }) {
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin=""
         />
-        {/* <link
+        <link
           rel="stylesheet"
           href="https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.css"
-        /> */}
-
+        />
         <script
           src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
           integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
           crossOrigin=""
           defer
         ></script>
-
-        {/* <script
+        <script
           src="https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.js"
           defer
-        ></script> */}
+        ></script>
       </head>
       <body
         className={cn(
