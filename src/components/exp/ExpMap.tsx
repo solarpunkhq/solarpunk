0466daa54c25642b872a7c82d530050cbed85b26
Varun Balani <varunprahladb@gmail.com>
2024-08-22 23:35:23 +0530

    add geoman control to exp map

--- src/components/exp/ExpMap.tsx
+++ src/components/exp/ExpMap.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import ReactLeafletGoogleLayer from 'react-leaflet-google-layer'
+import { GeomanControl } from '../map/GeomanControl'
 
 const ExpMap = () => {
   return (
@@ -11,9 +12,28 @@ const ExpMap = () => {
         scrollWheelZoom={true}
         style={{ width: '100vw', height: '100vh' }}
       >
-        <ReactLeafletGoogleLayer
-          type="hybrid"
-          apiKey="AIzaSyCyQtqLWjG5qYbwxLNjKoXZFci0nC5poaA"
+        <TileLayer
+          url={`https://{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}&scale=2&hl=en&gl=US&${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
+          subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+        />
+        <GeomanControl
+          position="topleft"
+          drawMarker={false}
+          drawPolyline={false}
+          measurement
+          drawCircle={false}
+          drawRectangle={false}
+          drawText={false}
+          dragMode={false}
+          cutPolygon={false}
+          splitMode={false}
+          scaleMode={false}
+          pinningOption={false}
+          snappingOption={false}
+          drawCircleMarker={false}
+          rotateMode={false}
+          snapGuidesOption={false}
+          autoTracingOption={false}
         />
       </MapContainer>
     </>
