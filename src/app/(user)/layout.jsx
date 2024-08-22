235dda37980108c187f187eaa323fdd845d0b9b3
Varun Balani <varunprahladb@gmail.com>
2024-08-22 23:31:32 +0530

    remove geoman script tags

--- src/app/(user)/layout.jsx
+++ src/app/(user)/layout.jsx
@@ -60,18 +60,10 @@ export default function Layout({ children }) {
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin=""
         />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.css"
-        />
         <script
           src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
           integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
           crossOrigin=""
-          defer
-        ></script>
-        <script
-          src="https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.js"
           defer
         ></script>
       </head>
