bd7b49080fd95498032706b2fad10cdd4ef8ce5c
Varun Balani <varunprahladb@gmail.com>
2024-08-25 12:12:26 +0530

    refactor projection calculator

--- src/components/ProjectionCalculator.tsx
+++ src/components/ProjectionCalculator.tsx
@@ -8,22 +8,17 @@ export default function ProjectionCalculator({ acres }: { acres: number }) {
   const [loanPeriod, setLoanPeriod] = useState(25)
   const [loanAPR, setLoanAPR] = useState(5)
 
-  const mw_produced = acres / 5
-  const build_cost = mw_produced * 1000000
-  const revenue_per_year = mw_produced * 175000
-  const annual_loan_payment =
-    ((build_cost * loanAPR) /
-      1200 /
-      (1 - Math.pow(1 + loanAPR / 1200, -loanPeriod * 12))) *
-    12
-
-  const solarpunk_revenue_per_year =
-    (revenue_per_year - annual_loan_payment - 7500) / 2
-
-  const total_solarpunk_revenue = solarpunk_revenue_per_year * loanPeriod
-  const total_revenue = revenue_per_year * loanPeriod
-  const total_borrowing_costs = annual_loan_payment * loanPeriod
-  const total_build_maintain_costs = build_cost + 7500 * loanPeriod
+  const {
+    mw_produced,
+    build_cost,
+    revenue_per_year,
+    annual_loan_payment,
+    solarpunk_revenue_per_year,
+    total_solarpunk_revenue,
+    total_revenue,
+    total_borrowing_costs,
+    total_build_maintain_costs,
+  } = getProjectionsFromAcres(acres, loanPeriod, loanAPR)
 
   return (
     <div>
---
+++ src/utils/projections.tsx
@@ -0,0 +1,34 @@
+const getProjectionsFromAcres = (
+  acres: number,
+  loanPeriod: number,
+  loanAPR: number
+) => {
+  const mw_produced = acres / 5
+  const build_cost = mw_produced * 1000000
+  const revenue_per_year = mw_produced * 175000
+  const annual_loan_payment =
+    ((build_cost * loanAPR) /
+      1200 /
+      (1 - Math.pow(1 + loanAPR / 1200, -loanPeriod * 12))) *
+    12
+
+  const solarpunk_revenue_per_year =
+    (revenue_per_year - annual_loan_payment - 7500) / 2
+
+  const total_solarpunk_revenue = solarpunk_revenue_per_year * loanPeriod
+  const total_revenue = revenue_per_year * loanPeriod
+  const total_borrowing_costs = annual_loan_payment * loanPeriod
+  const total_build_maintain_costs = build_cost + 7500 * loanPeriod
+
+  return {
+    mw_produced,
+    build_cost,
+    revenue_per_year,
+    annual_loan_payment,
+    solarpunk_revenue_per_year,
+    total_solarpunk_revenue,
+    total_revenue,
+    total_borrowing_costs,
+    total_build_maintain_costs,
+  }
+}
