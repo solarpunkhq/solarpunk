86e08909c95f261bdc680701f305f7758761ffcd
Olga <olga.volotovich@mail.ru>
2024-09-10 16:19:39 +0200

    chore: solar animation review

--- src/components/pages/home/become/animated-item/animated-item.tsx
+++ src/components/pages/home/become/animated-item/animated-item.tsx
@@ -8,7 +8,7 @@ import useWindowSize from '@/hooks/use-window-size';
 
 export type ItemType = 'text' | 'solar-animation';
 
-function useGetOpacity(
+function UseGetOpacity(
   index: number,
   totalItems: number,
   sectionRef: React.RefObject<HTMLDivElement>,
@@ -32,7 +32,7 @@ function UseGetContentOpacity(
   totalItems: number,
   sectionRef: React.RefObject<HTMLDivElement>,
 ) {
-  const opacity = useGetOpacity(index, totalItems, sectionRef);
+  const opacity = UseGetOpacity(index, totalItems, sectionRef);
   const contentOpacity = useTransform(opacity, (value) => (value <= 0.25 ? 0.25 : 1));
 
   return contentOpacity;
@@ -44,7 +44,7 @@ function UseCircleXPosition(
   sectionRef: React.RefObject<HTMLDivElement>,
   width: number | undefined,
 ) {
-  const opacity = useGetOpacity(index, totalItems, sectionRef);
+  const opacity = UseGetOpacity(index, totalItems, sectionRef);
 
   const startXPosition = useMemo(() => {
     if (!width) {
@@ -85,13 +85,54 @@ function UseCircleXPosition(
   return circleXPosition;
 }
 
+function UseBg(index: number, totalItems: number, sectionRef: React.RefObject<HTMLDivElement>) {
+  const opacity = UseGetOpacity(index, totalItems, sectionRef);
+  const circleColor = useTransform(
+    opacity,
+    [0.25, 0.251],
+    [
+      'linear-gradient(180deg, #FFF 0%, #FFF 117.52%)',
+      'linear-gradient(180deg, #FFDE6B 0%, #FF7511 117.52%)',
+    ],
+  );
+
+  return circleColor;
+}
+
+function UseUnderlayColor(
+  index: number,
+  totalItems: number,
+  sectionRef: React.RefObject<HTMLDivElement>,
+) {
+  const opacity = UseGetOpacity(index, totalItems, sectionRef);
+  const underlayColor = useTransform(opacity, [0.25, 0.251], ['#9194A1', '#040406']);
+
+  return underlayColor;
+}
+
+function UseBoxShadow(
+  index: number,
+  totalItems: number,
+  sectionRef: React.RefObject<HTMLDivElement>,
+) {
+  const opacity = UseGetOpacity(index, totalItems, sectionRef);
+
+  const boxShadow = useTransform(
+    opacity,
+    [0.25, 0.251],
+    ['0px 0px 8px 4px #9194A1', '0px 0px 8px 4px #000000'],
+  );
+
+  return boxShadow;
+}
+
 function UseTextX(
   index: number,
   totalItems: number,
   sectionRef: React.RefObject<HTMLDivElement>,
   width: number | undefined,
 ) {
-  const opacity = useGetOpacity(index, totalItems, sectionRef);
+  const opacity = UseGetOpacity(index, totalItems, sectionRef);
 
   const endXPosition = useMemo(() => {
     if (!width) {
@@ -105,7 +146,9 @@ function UseTextX(
     }
   }, [width]);
 
-  return useTransform(opacity, [0.25, 0.251], [0, endXPosition]);
+  const textXPosition = useTransform(opacity, [0.25, 0.251], [0, endXPosition]);
+
+  return textXPosition;
 }
 
 interface ItemProps {
@@ -139,24 +182,24 @@ function AnimatedItem({ content, index, totalItems, type, sectionRef }: ItemProp
                   <m.span
                     className="solar-text relative z-20 inline-block pl-14 font-sans text-55 leading-[0.8] text-white transition-all duration-500 lg:pl-[50px] lg:text-50 md:text-44 sm:pl-10 sm:text-30"
                     style={{
-                      opacity: UseGetContentOpacity(index, totalItems, sectionRef),
                       x: UseTextX(index, totalItems, sectionRef, width),
                     }}
                   >
                     solar
                   </m.span>
                   <m.span
-                    className="black-underlay absolute left-1/2 top-[54%] -z-50 h-[88px] w-[232px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-black transition-all duration-500 lg:h-[76px] lg:w-[200px] md:h-[66px] md:w-[185px] sm:h-11 sm:w-[126px]"
+                    className="black-underlay absolute left-1/2 top-[54%] -z-50 h-[88px] w-[232px] -translate-x-1/2 -translate-y-1/2 rounded-full transition-all duration-500 lg:h-[76px] lg:w-[200px] md:h-[66px] md:w-[185px] sm:h-11 sm:w-[126px]"
                     style={{
-                      opacity: UseGetContentOpacity(index, totalItems, sectionRef),
+                      backgroundColor: UseUnderlayColor(index, totalItems, sectionRef),
                     }}
                   />
                   <m.span
-                    className="orange-circle absolute -left-2.5 top-[54%] z-30 h-[54px] w-[54px] -translate-y-1/2 rounded-full bg-sun-icon-bg shadow-sun-icon transition-[transform,opacity,background] duration-[500ms,200ms,0ms] lg:-left-[26px] lg:size-[46px] md:size-[42px] sm:size-[30px] sm:translate-x-[23px]"
+                    className="orange-circle absolute -left-2.5 top-[54%] z-30 h-[54px] w-[54px] -translate-y-1/2 rounded-full transition-[transform,opacity,box-shadow] duration-[500ms,200ms,500ms] lg:-left-[26px] lg:size-[46px] md:size-[42px] sm:size-[30px] sm:translate-x-[23px]"
                     style={{
-                      opacity: UseGetContentOpacity(index, totalItems, sectionRef),
                       x: UseCircleXPosition(index, totalItems, sectionRef, width),
                       y: '-50%',
+                      background: UseBg(index, totalItems, sectionRef),
+                      boxShadow: UseBoxShadow(index, totalItems, sectionRef),
                     }}
                   />
                 </m.span>
