3aec3e523306386e4b571705df4d0ce10280af55
Olga <olga.volotovich@mail.ru>
2024-09-10 16:37:23 +0200

    feat: add og image and favicons

--- src/lib/get-metadata.ts
+++ src/lib/get-metadata.ts
@@ -6,9 +6,7 @@ type Metadata = {
   type?: string;
 };
 
-// FIXME: Add an image for displaying on social networks, remember that this image must meet the size of 1200x630
-// For example: /images/social-previews/index.jpg
-const DEFAULT_IMAGE_PATH = '';
+export const DEFAULT_IMAGE_PATH = '/images/og-image.png';
 
 export function getMetadata({
   title,
@@ -20,7 +18,7 @@ export function getMetadata({
   const SITE_URL = process.env.NEXT_PUBLIC_DEFAULT_SITE_URL;
   const canonicalUrl = SITE_URL + pathname;
   const imageUrl = imagePath.startsWith('http') ? imagePath : SITE_URL + imagePath;
-  const siteName = 'Solarpunk Next.js TypeScript Tailwind Starter';
+  const siteName = 'Solarpunk';
 
   return {
     title,
@@ -28,22 +26,21 @@ export function getMetadata({
     alternates: {
       canonical: canonicalUrl,
     },
-    // FIXME: Generate favicons with Favpie - https://github.com/pixel-point/favpie
-    // manifest: `${SITE_URL}/manifest.json`,
-    // icons: {
-    //   icon: '/favicon/favicon.png',
-    //   apple: [
-    //     { url: '/favicon/favicon.png' },
-    //     { url: '/favicon/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
-    //     { url: '/favicon/favicon-72x72.png', sizes: '72x72', type: 'image/png' },
-    //     { url: '/favicon/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
-    //     { url: '/favicon/favicon-144x144.png', sizes: '144x144', type: 'image/png' },
-    //     { url: '/favicon/favicon-180x180.png', sizes: '180x180', type: 'image/png' },
-    //     { url: '/favicon/favicon-256x256.png', sizes: '256x256', type: 'image/png' },
-    //     { url: '/favicon/favicon-384x384.png', sizes: '384x384', type: 'image/png' },
-    //     { url: '/favicon/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
-    //   ],
-    // },
+    manifest: `${SITE_URL}/manifest.json`,
+    icons: {
+      icon: '/favicon/favicon.png',
+      apple: [
+        { url: '/favicon/favicon.png' },
+        { url: '/favicon/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+        { url: '/favicon/favicon-72x72.png', sizes: '72x72', type: 'image/png' },
+        { url: '/favicon/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+        { url: '/favicon/favicon-144x144.png', sizes: '144x144', type: 'image/png' },
+        { url: '/favicon/favicon-180x180.png', sizes: '180x180', type: 'image/png' },
+        { url: '/favicon/favicon-256x256.png', sizes: '256x256', type: 'image/png' },
+        { url: '/favicon/favicon-384x384.png', sizes: '384x384', type: 'image/png' },
+        { url: '/favicon/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
+      ],
+    },
     openGraph: {
       title,
       description,
