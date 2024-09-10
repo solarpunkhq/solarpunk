37bca5bc2545dcf067870ffac9aaaf0a725cc099
Varun Balani <varunprahladb@gmail.com>
2024-09-11 03:05:54 +0530

    change onboarding ui

--- src/components/Onboarding.tsx
+++ src/components/Onboarding.tsx
@@ -1,14 +1,21 @@
 'use client'
 import dynamic from 'next/dynamic'
 import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Acre } from '@/utils/types'
 import { useRouter } from 'next/navigation'
 import { useSearchParams } from 'next/navigation'
-import { Loader2 } from 'lucide-react'
+import { CircleAlert, Loader2 } from 'lucide-react'
 import * as L from 'leaflet'
 console.log(L)
 import 'leaflet.gridlayer.googlemutant/dist/Leaflet.GoogleMutant'
+import { formatNumberAsAmount } from '@/lib/utils'
+import {
+  getProjectionsFromAcres,
+  getTotalAreaFromAcreData,
+} from '@/utils/projections'
+import { Logo } from './Logo'
+import { Input } from './ui/input'
 
 const Map = dynamic(() => import('@/components/map/Map'), {
   ssr: false,
@@ -16,6 +23,18 @@ const Map = dynamic(() => import('@/components/map/Map'), {
 
 export function OnboardingComponent({ country }: { country: string }) {
   const [acres, setAcres] = useState<Acre[]>([])
+  const [totalArea, setTotalArea] = useState(getTotalAreaFromAcreData(acres))
+  const [projections, setProjections] = useState(
+    getProjectionsFromAcres(totalArea, 25, 5)
+  )
+
+  useEffect(() => {
+    const newTotalArea = getTotalAreaFromAcreData(acres)
+    const newProjections = getProjectionsFromAcres(newTotalArea, 25, 5)
+    setTotalArea(newTotalArea)
+    setProjections(newProjections)
+  }, [acres])
+
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
 
@@ -90,84 +109,200 @@ export function OnboardingComponent({ country }: { country: string }) {
   }
 
   return (
-    <div className="-mb-[200px] mt-12 h-full">
+    // <div className="-mb-[200px] mt-12 h-full">
+    //   <div className="flex h-full w-full flex-col items-center justify-center px-8 md:flex-row md:items-stretch md:justify-start">
+    //     <Map
+    //       zoom={zoom}
+    //       lat={lat}
+    //       lng={lng}
+    //       acres={acres}
+    //       setAcres={setAcres}
+    //       existingAcres={[]}
+    //       country={country}
+    //     />
+    //     <div className="rounded-r-4xl h-full w-full max-w-96 border border-l-0 bg-white p-8">
+    //       <div>
+    //         <h1 className="font-display text-4xl">Mark your acres</h1>
+    //         <h2 className="">Use the map to outline your territory</h2>
+    //         <div className="relative flex overflow-x-auto">
+    //           <table className="mt-4 w-full border text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
+    //             <thead className="bg-gray-50 text-xs uppercase text-gray-700 ">
+    //               <tr>
+    //                 <th scope="col" className="px-6 py-3">
+    //                   Area
+    //                 </th>
+    //                 <th scope="col" className="px-6 py-3">
+    //                   Revenue/yr
+    //                 </th>
+    //               </tr>
+    //             </thead>
+    //             <tbody>
+    //               {acres.map((acre, index) => (
+    //                 <tr key={index} className="border-b bg-white ">
+    //                   <th
+    //                     scope="row"
+    //                     className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
+    //                   >
+    //                     Area {index + 1}
+    //                   </th>
+    //                   <td className="px-6 py-4">
+    //                     ${acre.revenue.toLocaleString()}/yr
+    //                   </td>
+    //                 </tr>
+    //               ))}
+    //               {acres.length === 0 && (
+    //                 <tr className="border-b bg-white ">
+    //                   <th
+    //                     scope="row"
+    //                     className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
+    //                   >
+    //                     Select an area
+    //                   </th>
+    //                   <td className="px-6 py-4">$xxx,yyy/yr</td>
+    //                 </tr>
+    //               )}
+    //             </tbody>
+    //           </table>
+    //         </div>
+    //         <input
+    //           className="mt-4 w-full rounded border p-2"
+    //           placeholder="Email"
+    //           onChange={(e) => setEmail(e.target.value)}
+    //           value={email}
+    //         />
+    //         <input
+    //           className="mt-4 w-full rounded border p-2"
+    //           placeholder="Name"
+    //           onChange={(e) => setName(e.target.value)}
+    //           value={name}
+    //         />
+    //         {error && <div className="mt-2 text-red-500">{error}</div>}
+    //         <Button
+    //           className="mx-auto mt-4 !px-5 !py-3"
+    //           onClick={() => {
+    //             submitForm()
+    //           }}
+    //           disabled={loading}
+    //         >
+    //           {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+    //           {loading ? 'Please wait' : 'Submit'}
+    //         </Button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="mt-4 h-screen w-full overflow-hidden">
       <div className="flex h-full w-full flex-col items-center justify-center px-8 md:flex-row md:items-stretch md:justify-start">
-        <Map
-          zoom={zoom}
-          lat={lat}
-          lng={lng}
-          acres={acres}
-          setAcres={setAcres}
-          existingAcres={[]}
-          country={country}
-        />
-        <div className="rounded-r-4xl h-full w-full max-w-96 border border-l-0 bg-white p-8">
-          <div>
-            <h1 className="font-display text-4xl">Mark your acres</h1>
-            <h2 className="">Use the map to outline your territory</h2>
-            <div className="relative flex overflow-x-auto">
-              <table className="mt-4 w-full border text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
-                <thead className="bg-gray-50 text-xs uppercase text-gray-700 ">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">
-                      Area
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Revenue/yr
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {acres.map((acre, index) => (
-                    <tr key={index} className="border-b bg-white ">
-                      <th
-                        scope="row"
-                        className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
-                      >
-                        Area {index + 1}
-                      </th>
-                      <td className="px-6 py-4">
-                        ${acre.revenue.toLocaleString()}/yr
-                      </td>
-                    </tr>
-                  ))}
-                  {acres.length === 0 && (
-                    <tr className="border-b bg-white ">
-                      <th
-                        scope="row"
-                        className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 "
-                      >
-                        Select an area
-                      </th>
-                      <td className="px-6 py-4">$xxx,yyy/yr</td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
-            <input
-              className="mt-4 w-full rounded border p-2"
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-            />
-            <input
-              className="mt-4 w-full rounded border p-2"
-              placeholder="Name"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-            />
-            {error && <div className="mt-2 text-red-500">{error}</div>}
-            <Button
-              className="mx-auto mt-4 !px-5 !py-3"
-              onClick={() => {
-                submitForm()
-              }}
-              disabled={loading}
+        <div className="flex h-full w-full flex-col items-center justify-start">
+          <div className="mb-8 ml-6 mt-4 flex items-center justify-center self-start">
+            <div
+              className="flex cursor-pointer items-center"
+              onClick={() => (window.location.href = '/')}
             >
-              {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-              {loading ? 'Please wait' : 'Submit'}
-            </Button>
+              <Logo filled={true} className="h-8" />
+            </div>
+          </div>
+          <Map
+            zoom={zoom}
+            lat={lat}
+            lng={lng}
+            acres={acres}
+            setAcres={setAcres}
+            existingAcres={[]}
+            country={country}
+          />
+          <div className="mt-2 w-1/2 rounded-md bg-neutral-100 p-6 text-center text-sm">
+            <span className="font-semibold">
+              ${formatNumberAsAmount(projections.revenue_per_year.toFixed(0))}{' '}
+              Est. Yearly Revenue
+            </span>
+            {' | '}
+            <span className="text-gray-600">
+              {' '}
+              {formatNumberAsAmount(totalArea.toFixed(2))} Acres
+            </span>
+          </div>
+        </div>
+        <div className="rounded-r-4xl ml-4 h-full max-h-screen w-full max-w-96 bg-white">
+          <div className="mt-20 flex flex-col border-2 border-gray-200 p-4">
+            <h1 className="mb-4 text-3xl font-bold">Mark your land</h1>
+            <div className="relative mb-4 flex-col overflow-x-auto text-sm">
+              <div className="mb-4">
+                Use the map to select your territory. Use the tools on the left
+                of the map to mark your land:{' '}
+              </div>
+              <div className="flex flex-row items-center justify-start gap-2">
+                <img src="/map_icons/map_type_tool.svg"></img>
+                <span>Toggle between satellite and roadmap views</span>
+              </div>
+              <div className="flex flex-row items-center justify-start gap-2">
+                <img src="/map_icons/zoom_in_tool.svg"></img>
+                <span>Zoom in to the map</span>
+              </div>
+              <div className="flex flex-row items-center justify-start gap-2">
+                <img src="/map_icons/zoom_out_tool.svg"></img>
+                <span>Zoom out of the map</span>
+              </div>
+              <div className="flex flex-row items-center justify-start gap-2">
+                <img src="/map_icons/vertex_tool.svg"></img>
+                <span>Add a new vertex or selection</span>
+              </div>
+              <div className="flex flex-row items-center justify-start gap-2">
+                <img src="/map_icons/edit_vertex_tool.svg"></img>
+                <span>Edit an existing vertex or selection</span>
+              </div>
+              <div className="flex flex-row items-center justify-start gap-2">
+                <img src="/map_icons/erase_selection_tool.svg"></img>
+                <span>Erase a selection</span>
+              </div>
+              <div className="flex flex-row items-center justify-start gap-2">
+                <img src="/map_icons/merge_selections_tool.svg"></img>
+                <span>Merge two selections</span>
+              </div>
+
+              <div className="mt-4 text-2xl font-semibold">Your Details</div>
+              <div className="ml-1 text-base font-semibold">Name</div>
+              <Input
+                placeholder="Your name"
+                className="!ml-1 !w-[97%] !border-gray-300"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <div className="ml-1 mt-4 text-base font-semibold">Email</div>
+              <Input
+                placeholder="Your email"
+                className="!ml-1 !w-[97%] !border-gray-300"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <div className="mt-4 text-2xl font-semibold">Summary</div>
+              <div>
+                <b>Area: </b>
+                {formatNumberAsAmount(totalArea.toFixed(2))} Acres
+              </div>
+              <div>
+                <b>Est. Revenue: </b>$
+                {formatNumberAsAmount(projections.revenue_per_year.toFixed(0))}
+                /yr
+              </div>
+              <div>
+                <b>Est. Energy Production: </b>
+                {formatNumberAsAmount(projections.mw_produced.toFixed(1))} MW/yr
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                {error && <div className=" text-red-500">{error}</div>}
+                <Button
+                  className="mt-4"
+                  disabled={loading}
+                  onClick={() => {
+                    submitForm()
+                  }}
+                >
+                  {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                  {loading ? 'Please wait' : 'Submit'}
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
