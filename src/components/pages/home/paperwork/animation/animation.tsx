d5ba1d2a129625b551c986628806cd0dee9a1f18
Olga <olga.volotovich@mail.ru>
2024-09-13 17:18:04 +0200

    refactor: disable hover on animations for mobiles in paperwork

--- src/components/pages/home/paperwork/animation/animation.tsx
+++ src/components/pages/home/paperwork/animation/animation.tsx
@@ -6,6 +6,7 @@ import clsx from 'clsx';
 import RiveAnimation from '@/components/shared/rive-animation';
 
 import useRiveAnimation from '@/hooks/use-rive-animation';
+import useWindowSize from '@/hooks/use-window-size';
 
 function Animation({ src, className }: { src: string; className?: string }) {
   const { riveInstance, wrapperRef, animationRef, isIntersecting, setRiveInstance } =
@@ -23,12 +24,16 @@ function Animation({ src, className }: { src: string; className?: string }) {
       riveInstance?.resizeDrawingSurfaceToCanvas();
     },
   };
+  const { width } = useWindowSize();
+  const isMobile = (width ?? 0) <= 768;
 
   return (
     <div className="absolute inset-0 h-full w-full">
       <span className="absolute left-0 top-0 -z-10 h-full w-px" ref={wrapperRef} aria-hidden />
       <div
-        className={clsx('relative h-full w-full [&_canvas]:!h-full [&_canvas]:!w-full', className)}
+        className={clsx('relative h-full w-full [&_canvas]:!h-full [&_canvas]:!w-full', className, {
+          'pointer-events-none': isMobile,
+        })}
         ref={animationRef}
       >
         {isIntersecting ? (
