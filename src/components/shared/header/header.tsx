4db2d07ff5515475efa608475e0e8e7164d865bc
Olga <olga.volotovich@mail.ru>
2024-09-16 11:50:57 +0200

    refactor: header adaptive width

--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -20,7 +20,10 @@ function Header() {
   return (
     <>
       <header className="absolute left-0 right-0 top-0 z-50 h-14 bg-white px-safe pt-safe sm:h-[42px]">
-        <nav className="container flex h-full items-center justify-between" aria-label="Global">
+        <nav
+          className="container-wide flex h-full items-center justify-between"
+          aria-label="Global"
+        >
           <Link href={ROUTE.index}>
             <Image src={logo} width={115} height={26} alt="" priority />
             <span className="sr-only">Solarpunk</span>
