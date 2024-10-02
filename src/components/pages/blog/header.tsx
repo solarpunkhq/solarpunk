a22a25c96d724ecb62b8fe2284e8de1d2c486a00
Varun Balani <varunprahladb@gmail.com>
2024-10-03 02:19:59 +0530

    fix: bug fix for href

--- src/components/pages/blog/header.tsx
+++ src/components/pages/blog/header.tsx
@@ -3,10 +3,12 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <h2 className="mb-20 mt-8 flex items-center text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
-      <Link href="/blog" className="hover:underline">
-        SolarpunkHQ
-      </Link>
-      .
+      {
+        //@ts-ignore
+        <Link href="/blog" className="hover:underline">
+          SolarpunkHQ.
+        </Link>
+      }
     </h2>
   );
 };
