bc2204cb9012fa83db953bba8a0e01ff8a0c12ae
Peer Richelsen <peer@cal.com>
2024-10-03 20:25:08 +0200

    nit

--- src/constants/menus.ts
+++ src/constants/menus.ts
@@ -3,16 +3,14 @@ import { ROUTE } from '@/constants/route';
 export const MENUS = {
   header: [
     { label: 'Agrivoltaics', href: ROUTE.agrivoltaics },
-    { label: 'Blog', href: ROUTE.blog },
-    { label: 'Jobs', href: ROUTE.index },
-    { label: 'Contact us', href: ROUTE.contactUs },
+    { label: 'Blog', href: ROUTE.blog } /* 
+    { label: 'Jobs', href: ROUTE.index }, */,
   ],
   footer: {
     main: [
       { label: 'Agrivoltaics', href: ROUTE.agrivoltaics },
-      { label: 'Blog', href: ROUTE.blog },
-      { label: 'Jobs', href: ROUTE.index },
-      { label: 'Contact us', href: ROUTE.contactUs },
+      { label: 'Blog', href: ROUTE.blog } /* 
+      { label: 'Jobs', href: ROUTE.index }, */,
     ],
   },
 };
