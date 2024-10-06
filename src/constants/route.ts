62e6638e92ced44c14a3a4ff91c7399fc839067e
Peer Richelsen <peeroke@gmail.com>
2024-10-06 16:48:36 +0200

    Update route.ts

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -5,6 +5,8 @@ export const ROUTE: Record<string, URL | Route<string>> = {
   agrivoltaics: '/blog/agrivoltaics' as Route<string>,
   contactUs: 'https://cal.com/team/solarpunk/exploration',
   terms: '/terms',
+  x: 'https://x.com/solarpunkhq',
+  github: 'https://github.com/solarpunkhq/solarpunk',
   privacy: '/privacy' as Route<string>,
   blog: '/blog',
   login: '/login',
