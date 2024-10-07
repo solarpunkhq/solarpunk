b99d77435d85cf6e9e6f30eba10b1e77b80ca81e
Peer Richelsen <peer@cal.com>
2024-10-07 13:32:00 +0200

    chore: login layout

--- src/app/(user)/login/layout.tsx
+++ src/app/(user)/login/layout.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
+import { inter, manrope } from '@/fonts';
+
+import Footer from '@/components/shared/footer';
 import Header from '@/components/shared/header';
+
+import '@/styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
-      {children}
+      <div className="flex min-h-full flex-col justify-center">
+        <Header />
+        {children}
+      </div>
     </>
   );
 }
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+  themeColor: '#000000',
+};
