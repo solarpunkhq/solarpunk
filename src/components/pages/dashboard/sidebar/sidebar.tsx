fa0e6871c62653f70a58b934cd7458acd4ef93ad
Varun Balani <varunprahladb@gmail.com>
2024-10-13 19:11:12 +0530

    fix: submit button changes

--- src/components/pages/dashboard/sidebar/sidebar.tsx
+++ src/components/pages/dashboard/sidebar/sidebar.tsx
@@ -76,14 +76,13 @@ function DashboardSidebar({ user_id, acres }: { user_id: number; acres: any[] })
   };
 
   return (
-    <div className="relative mb-4 mt-4 flex h-[90vh] flex-col overflow-hidden rounded-xl border-2 border-gray-80 bg-gray-98 p-4 text-14">
-      <div className="flex-1 overflow-y-auto">
+    <div className="relative mb-4 mt-4 flex h-[90vh] flex-col overflow-hidden rounded-xl border-2 border-gray-80 bg-gray-98 text-14">
+      <div className="flex-1 overflow-y-auto p-4">
         <h1 className="mb-4 font-title text-28 font-bold">Your Acres</h1>
         <div className="mb-4">
           Please submit additional information about your land. This information will help us
           process your application faster.
         </div>
-        <Schedule />
         <div className="my-4 text-20 font-semibold">Personal Details</div>
         <div className="ml-1 text-15 font-semibold">Phone Number</div>
         <Input
@@ -287,20 +286,27 @@ function DashboardSidebar({ user_id, acres }: { user_id: number; acres: any[] })
           </span>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center pt-4">
+      <div className="-mb-[10px] flex flex-col items-center justify-start rounded-b-2xl bg-gray-20 py-4">
         {error && <div className="text-primary-red">{error}</div>}
-        <Button
-          className="flex w-full items-center justify-center rounded-md px-6 py-2"
-          theme="black"
-          onClick={submitForm}
-        >
-          {loading && (
-            <div className="px-4 py-1">
-              <Loader2 className="h-4 w-4 animate-spin" />
-            </div>
-          )}
-          {loading ? '' : 'Submit'}
-        </Button>
+        <div className="flex gap-2 pb-2">
+          <Button size="home-md" theme="black" href="https://cal.com/team/solarpunk/exploration">
+            Contact us
+          </Button>
+          <Button
+            className="flex items-center justify-center !px-8"
+            size="home-md"
+            theme="green"
+            disabled={phoneNumber.length === 0}
+            onClick={submitForm}
+          >
+            {loading && (
+              <div className="px-6">
+                <Loader2 className="h-4 w-4 animate-spin" />
+              </div>
+            )}
+            {loading ? '' : 'Submit'}
+          </Button>
+        </div>
       </div>
     </div>
   );
