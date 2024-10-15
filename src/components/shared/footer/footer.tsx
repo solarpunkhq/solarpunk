151551354e8375982c8fa985ec401f0d1c8da163
Varun Balani <varunprahladb@gmail.com>
2024-10-16 01:00:42 +0530

    chore: footer translations

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
 import Button from '@/components/shared/button';
@@ -7,20 +10,20 @@ import { MENUS } from '@/constants/menus';
 import { ROUTE } from '@/constants/route';
 
 function Footer() {
+  const t = useTranslations('FooterComponent');
+
   return (
     <footer className="relative bg-gray-12 py-[26px] px-safe home-sm:py-4">
       <nav className="container flex h-full items-center justify-between gap-1 px-4 home-sm:flex-col home-sm:items-start home-sm:gap-6">
         <Logo invert />
         <ul className="grid grid-cols-5 items-center justify-between uppercase leading-none md:gap-2">
-          {MENUS.footer.main.map(({ label, href }, index) => {
-            return (
-              <li key={index}>
-                <Button className="p-1 font-semibold tracking-wide" href={href} theme="black">
-                  {label}
-                </Button>
-              </li>
-            );
-          })}
+          {MENUS.footer.main.map(({ label, href }, index) => (
+            <li key={index}>
+              <Button className="p-1 font-semibold tracking-wide" href={href} theme="black">
+                {label}
+              </Button>
+            </li>
+          ))}
         </ul>
         <Button
           className="mr-[7px] shrink-0 home-lg:mr-0"
@@ -30,14 +33,14 @@ function Footer() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Contact us
+          {t('contactUs')}
         </Button>
       </nav>
       <p className="px-4 pt-4 text-xs text-gray-50 sm:text-center">
-        Donate on Bittensor:{' '}
+        {t('donateText')}{' '}
         <a
           target="_blank"
-          className="text-gray-100 break-all underline hover:text-white "
+          className="text-gray-100 break-all underline hover:text-white"
           href="https://bittensor.com/scan/address/5HZHcpjgozBVD9vHRTBo1fvtrNHXvq49ZpCre3t51siXmr15"
         >
           5HZHcpjgozBVD9vHRTBo1fvtrNHXvq49ZpCre3t51siXmr15
