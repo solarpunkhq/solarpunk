b83926e4996dfe1248980e96ccd905053c4a7818
Varun Balani <varunprahladb@gmail.com>
2024-10-16 00:54:05 +0530

    chore: quote translations

--- src/components/pages/home/quote/quote.tsx
+++ src/components/pages/home/quote/quote.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
+
 import React, { useEffect, useRef } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 function Quote() {
+  const t = useTranslations('QuoteComponent');
   const firstHighlightRef = useRef<HTMLSpanElement | null>(null);
   const secondHighlightRef = useRef<HTMLSpanElement | null>(null);
   const { ref, inView } = useInView({
@@ -31,20 +34,18 @@ function Quote() {
       <div className="container">
         <figure className="relative flex max-w-[1344px] justify-between gap-2 home-sm:flex-col home-sm:gap-5">
           <figcaption className="fs-24 mt-2 leading-none tracking-tighter text-gray-40 home-lg:mt-3 home-md:mt-0">
-            – Thomas Edison
+            {t('author')}
           </figcaption>
           <blockquote className="fs-28 max-w-[738px] font-medium leading-snug tracking-tighter home-lg:max-w-[640px] home-md:max-w-[448px] home-sm:max-w-none">
-            “We are like tenant farmers{' '}
+            {t('quote').split(t('firstHighlight'))[0]}
             <mark className="highlight" ref={firstHighlightRef}>
-              chopping down
-            </mark>{' '}
-            the fence around our house for fuel when we should be using nature&apos;s inexhaustible
-            sources of energy –{' '}
+              {t('firstHighlight')}
+            </mark>
+            {t('quote').split(t('firstHighlight'))[1].split(t('secondHighlight'))[0]}
             <mark className="highlight" ref={secondHighlightRef}>
-              sun, wind and tide.
-            </mark>{' '}
-            I&apos;d put my money on the sun and solar energy. What a source of power! I hope we
-            don&apos;t have to wait until oil and coal run out before we tackle that.”
+              {t('secondHighlight')}
+            </mark>
+            {t('quote').split(t('secondHighlight'))[1]}
           </blockquote>
         </figure>
       </div>
