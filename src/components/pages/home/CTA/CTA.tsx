d4772eb7f20d39db64b33a6e6c3aa63ca1b1b786
Varun Balani <varunprahladb@gmail.com>
2024-10-16 00:52:29 +0530

    chore: cta translations

--- src/components/pages/home/CTA/CTA.tsx
+++ src/components/pages/home/CTA/CTA.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { Route } from 'next';
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
 import Button from '@/components/shared/button';
@@ -11,18 +14,19 @@ import greyBgImage from '@/images/cta/gray-bg-blured.jpg';
 import Card from './card';
 
 function CTA() {
+  const t = useTranslations('CTAComponent');
+
   return (
     <section className="cta mt-[230px] px-safe home-xl:mt-[168px] home-lg:mt-[155px] home-md:mt-[136px] home-sm:mt-[104px]">
       <div className="container home-xl:max-w-5xl">
         <div className="flex items-center justify-between gap-8 home-md:flex-col home-md:gap-6 home-sm:gap-6">
           <Card secondaryBg="bg-cta-card-bg home-sm:bg-cta-card-bg-sm">
             <h2 className="font-title text-44 font-semibold leading-[1.2] tracking-tight text-white home-xl:text-40 home-lg:text-37 home-md:max-w-[500px] home-md:text-29 home-sm:text-26">
-              Deploy solar installations with Solarpunk in weeks, not&nbsp;years
+              {t('card1Title')}
             </h2>
-            <div className=" flex items-end justify-between gap-3.5 home-xl:flex-col home-xl:items-start home-md:flex-row home-md:items-end home-sm:flex-col home-sm:items-start home-sm:gap-y-5">
+            <div className="flex items-end justify-between gap-3.5 home-xl:flex-col home-xl:items-start home-md:flex-row home-md:items-end home-sm:flex-col home-sm:items-start home-sm:gap-y-5">
               <p className="fs-20 max-w-[480px] font-medium leading-snug tracking-tighter text-white home-xl:max-w-[404px] home-md:max-w-[452px] home-sm:text-16">
-                We will help you understand what type of Agrivoltaic project is right for you and
-                what type of regulatory hurdles you will face.
+                {t('card1Description')}
               </p>
               <Button
                 className="group shrink-0 gap-0.5 home-lg:gap-0 home-sm:gap-0"
@@ -31,7 +35,7 @@ function CTA() {
                 href={'/' as Route}
                 withArrow
               >
-                Get started
+                {t('card1ButtonText')}
               </Button>
               <Image
                 className="absolute inset-0 -z-10 h-full w-full translate-x-[13px] scale-[1.135] object-cover object-center home-lg:translate-x-[59px] home-lg:translate-y-[-11px] home-lg:scale-[1.44] home-md:translate-x-[-25px] home-md:translate-y-[-27px] home-md:scale-[1.213] home-sm:translate-x-[61px] home-sm:translate-y-[-55px] home-sm:scale-[1.75]"
@@ -44,11 +48,11 @@ function CTA() {
           </Card>
           <Card className="home-sm:min-h-[383px] home-sm:justify-start home-sm:gap-y-2">
             <h2 className="max-w-[430px] font-title text-60 font-semibold leading-[1.16] tracking-tight text-gray-20 home-xl:max-w-[392px] home-xl:text-52 home-md:max-w-[352px] home-md:text-45 home-sm:text-30">
-              Not interested in agrivoltaics?
+              {t('card2Title')}
             </h2>
-            <div className=" flex items-end justify-between gap-3.5 home-xl:flex-col home-xl:items-start home-md:flex-row home-md:items-end home-sm:flex-col home-sm:items-start home-sm:gap-y-5">
+            <div className="flex items-end justify-between gap-3.5 home-xl:flex-col home-xl:items-start home-md:flex-row home-md:items-end home-sm:flex-col home-sm:items-start home-sm:gap-y-5">
               <p className="fs-20 max-w-[327px] font-medium leading-snug tracking-tighter text-gray-50 home-xl:max-w-[299px] home-md:max-w-[372px] home-md:text-20 home-sm:max-w-[245px] home-sm:text-16">
-                We also help with open space solar deployment without agriculture.
+                {t('card2Description')}
               </p>
               <Button
                 className="group shrink-0 gap-0.5 home-lg:gap-0 home-sm:gap-0"
@@ -57,7 +61,7 @@ function CTA() {
                 href={ROUTE.contactUs}
                 withArrow
               >
-                Contact us
+                {t('card2ButtonText')}
               </Button>
             </div>
             <Image
