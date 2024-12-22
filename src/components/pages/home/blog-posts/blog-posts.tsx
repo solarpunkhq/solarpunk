8de48106a6dcce716b6ec0c7b79c4cdd98331321
Esther Lizardo <100808713+Esther-Lita@users.noreply.github.com>
2024-12-22 21:51:49 +0100

    Update blog display in home

--- src/components/pages/home/blog-posts/blog-posts.tsx
+++ src/components/pages/home/blog-posts/blog-posts.tsx
@@ -1,5 +1,6 @@
 import { useLocale } from 'next-intl';
 
+import Button from '@/components/shared/button';
 import Container from '@/components/shared/container';
 
 import { getAllPosts } from '@/lib/blog-api';
@@ -14,8 +15,8 @@ export default function BlogPosts() {
   return (
     <Container>
       <BlogHeader />
-      <div className="grid-col-1 mb-28 grid gap-x-4 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
-        {allPosts.map((post) => (
+      <div className="grid-col-1 mb-16 grid gap-x-4 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
+        {allPosts.slice(0, 3).map((post) => (
           <PostItem
             key={post.slug}
             title={post.title}
@@ -23,8 +24,11 @@ export default function BlogPosts() {
             slug={post.slug}
             excerpt={post.excerpt}
           />
-        ))}
+        ))}{' '}
       </div>
+      <Button className="mb-28 " size="home-md" theme="green" href="/blog" withArrow>
+        Read more
+      </Button>
     </Container>
   );
 }
