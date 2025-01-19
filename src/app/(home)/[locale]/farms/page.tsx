44c682a776716431f9ae475a14d87b0c899b09f5
Peer Richelsen <peer@cal.com>
2025-01-19 21:00:41 +0100

    feat: farms page

--- src/app/(home)/[locale]/farms/page.tsx
+++ src/app/(home)/[locale]/farms/page.tsx
@@ -1,20 +1,34 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import Container from '@/components/shared/container';
 
 export default function FarmsPage() {
   return (
     <Container>
-      <div className="my-12 grid h-full grid-cols-2">
-        <div>
+      <div
+        className="my-12
+       "
+      >
+        <Link href="/en/farms/0001">
+          <Image
+            className="mb-4 rounded-xl"
+            src="/studio-1.jpg"
+            alt="Studio"
+            width={1270}
+            height={555}
+          />
+        </Link>
+
+        <Link href="/en/farms/0001">
           <Image
             className="rounded-xl"
-            src="/studio-1.jpg"
-            alt="Farm 1"
-            width={1000}
-            height={1000}
+            src="/1br-1.jpg"
+            alt="1 Bedroom"
+            width={1270}
+            height={555}
           />
-        </div>
+        </Link>
       </div>
     </Container>
   );
